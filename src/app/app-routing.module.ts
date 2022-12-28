f79fc2aca44acf3cd0b5bc73e7a44bb811d93c6a
Matheus Silva <matheussgsilva.com>
2022-12-28 18:10:18 -0300

    Create the landing page header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 
 const routes: Routes = [
-  { path:'', component: LayoutComponent }
+  { path:'', component: LayoutComponent, children:[
+    { path:'', component: LandingPageComponent }
+  ] }
 ];
 
 @NgModule({
