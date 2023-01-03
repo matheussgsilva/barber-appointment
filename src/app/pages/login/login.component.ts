248381d099f021a603bf3391e5523705437f9da7
Matheus Silva <matheussgsilva.com>
2023-01-03 20:07:46 -0300

    Create function login - with bug

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
+
+import { User } from 'src/app/models/user';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,13 +13,24 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent {
 
+  constructor(
+    private apiService: ApiService,
+    private authService: AuthService,
+  ) {}
+
   public form: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.required]),
-    password: new FormControl('', Validators.required)
+    email: new FormControl('matheus.silva@gmail.com', [Validators.required]),
+    password: new FormControl('123456', Validators.required)
   })
 
   public login(): void {
-    
+    if(this.form.valid) {
+      this.apiService.post<User>('sessions', this.form.value)
+      .then(data => {this.authService.login(data)})
+      .catch(response => alert(`Atenção', ${response.error.error}`))
+    } else {
+      alert('Atenção, Preencha o formulário antes de continuar')
+    }
   }
 
 }
